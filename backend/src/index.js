d4db3f3780e7ac3400c4c903d514b178a9818f7e
Gabriela Dalila Contrera <dalilagcontrera@live.com.ar>
2023-12-05 20:45:10 -0300

    se crea modelo de usuario,regsitro y login, tambien sus rutas

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,9 +1,10 @@
 import { app } from "./app.js";
 import {settingDotEnvPort} from "./config/dotenv.js";
+import {connectMongo} from "./database/db.js";
 
 const {port} = settingDotEnvPort()
 
-
+connectMongo()
 
 app.listen(port, console.log(`Servidor en puerto ${port}`));
 
