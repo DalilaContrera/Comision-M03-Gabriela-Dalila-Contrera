352f9bba039b2e02ec1de94220878826ae7e3570
Gabriela Dalila Contrera <dalilagcontrera@live.com.ar>
2023-12-15 20:10:03 -0300

    Se comienza a trabajar el front se crea formulario de registro

---
+++ frontend/src/App.jsx
@@ -0,0 +1,16 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Register } from "./pages/Register";
+
+export const App = () => {
+
+return(
+   <Router>
+     <Routes>
+        <Route path="/" element={<h1>Home</h1>}/>
+        <Route path="/login" element={<h1>Login</h1>}/>
+        <Route path="/register" element={<Register/> }/>
+        <Route path="/profile" element={<h1>Profile</h1>}/>
+     </Routes>
+   </Router>
+  );
+};
